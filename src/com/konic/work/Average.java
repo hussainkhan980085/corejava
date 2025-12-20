72c36bfca14dc19115c8592fe36690803dab8cb2
hussainkhan980085 <hussainkhan980085@gmail.com>
2025-12-20 15:39:23 +0530

    commit

---
+++ src/com/konic/work/Average.java
@@ -0,0 +1,21 @@
+package com.konic.work;
+
+public class Average {
+	
+	public static void main(String[] args) {
+		
+		int [] a = {10,20,30,40};
+		int sum = 0;
+		
+				
+		for (int i =0;i<a.length;i++) {
+			sum = sum + a[i];
+					;
+		}
+		double average = (double)sum/a.length;
+		
+		System.out.println("Total marks:"+sum);
+		System.out.println("Average marks:"+ average);
+	}
+
+}
--- src/com/konic/work/StoreMarks.java
+++ src/com/konic/work/StoreMarks.java
@@ -11,7 +11,7 @@ public class StoreMarks {
 	for(int i = 0;i<a.length;i++) {
 		
 	
-	System.out.print(a[i]+" ");
+	System.out.print(a[i]+" "); 
 	}
 	}
 }
