026f351d45deed3aaedc91e9d89b9d64ca38d254
hussainkhan980085 <hussainkhan980085@gmail.com>
2025-12-24 19:13:53 +0530

    Commit

---
+++ src/com/konic/java/constructors/cstudent.java
@@ -0,0 +1,38 @@
+package com.konic.java.constructors;
+
+public class cstudent {
+	
+	
+   String name;
+   int rollno;
+   
+   static String collegename = "IIT Delhi";
+   
+   cstudent(String name,int rollno){
+	   this.name=name;
+	   this.rollno=rollno;
+   }
+	   
+	   static void changeCollege(String cname) {
+		  collegename = cname;
+		  
+	  }
+    void display() {
+    	System.out.println("college name:"+collegename);
+    	System.out.println("student:"+name);
+    	System.out.println("rollno:"+rollno);
+    	
+    }
+   
+   
+	public static void main(String[] args) {
+		
+		cstudent.changeCollege("IIT Delhi");
+	
+		cstudent std=new cstudent("zara",12);
+		std.display();
+		
+
+	}
+
+}
